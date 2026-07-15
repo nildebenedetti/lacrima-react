b34b01340e905f59bd41af53ecced24caf64c1ae
“Nilde <nilde.benedetti@gmail.com>
2026-07-15 10:25:40 +0200

    Replace Montserrat font with Manrope and update styles; add new font file

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <div className="main d-flex justify-content-center align-items-center">
       <div className="title-wrapper">
         <h1 className="p-5">Hello Lacrima!</h1>
+        <p>Beauty belongs to those who still dare to feel.</p>
       </div>
     </div>
   </>
