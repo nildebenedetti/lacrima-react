c28506a15de133b28c9d3839ba90b1be17f1196f
“Nilde <nilde.benedetti@gmail.com>
2026-07-15 15:45:27 +0200

    added bootstrap and global CSS file  imports to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+/* bootstrap */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+/* CSS global */
+import './index.css';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
