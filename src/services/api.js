f5d9799887a79fdd0091b37d540d7de30c4244c2
“Nilde <nilde.benedetti@gmail.com>
2026-07-20 12:49:13 +0200

    feat(api): create API connection and implement getProducts function

---
+++ src/services/api.js
@@ -0,0 +1,30 @@
+const URL = import.meta.env.VITE_SERVER_URL || "localhost";
+const PORT = import.meta.env.VITE_SERVER_PORT || 3000;
+
+const API_URL = `http://${URL}:${PORT}`;
+
+const api = {
+    async getProducts() {
+            try {
+                const response = await fetch(`${API_URL}/products`);
+
+                if (!response.ok) {
+                    throw new Error("An error occourred when loading data", error);
+                }
+
+                const data = await response.json();
+
+                if (data.error) {
+                    throw new Error(data.error);
+                }
+
+                return data.result || data;
+
+            } catch (error) {
+
+            console.error("error loading products form database")
+            }
+
+        },
+}
+export default api;
