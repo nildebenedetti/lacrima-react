3157b03b40e9a052b9debc18c902ddb091649b8a
“Nilde <nilde.benedetti@gmail.com>
2026-07-20 10:50:04 +0200

    fix(home): adjusted text alignment in placeholder text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Home() {
     return <>
         <div className="main d-flex justify-content-center align-items-center">
-            <div className="title-wrapper">
+            <div className="title-wrapper text-center">
                 <h1 className="p-5">Hello Lacrima!</h1>
                 <p>Beauty belongs to those who still dare to feel.</p>
             </div>
