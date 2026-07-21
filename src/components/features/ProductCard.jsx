772a4ecbb858a4de83372088519e05676a2e9ad9
“Nilde <nilde.benedetti@gmail.com>
2026-07-21 18:10:42 +0200

    feat(ProductCard): add 'new badge' and brand line name + related style

--- src/components/features/ProductCard.jsx
+++ src/components/features/ProductCard.jsx
@@ -8,11 +8,13 @@ import { useState } from "react";
 
 function ProductCard({ product }) {
     const [isHovered, setIsHovered] = useState(false);
+    const [isNew, setIsNew] = useState(false);
 
     return <>
         <div className="card-wrapper col-sm-12 col-md-4 col-lg-3">
             <div className={styles.productCard}>
                 <div className={styles.cardHeader}>
+                    <span className={`badge ${styles.newBadge}`}>New</span>
                     <span className={`${styles.favHeart} icon`} onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
                         {isHovered ? <FaHeart color="#b388ff" /> : <LuHeart />}
                         </span>
@@ -20,12 +22,14 @@ function ProductCard({ product }) {
                 <Link to={`/products/${product.slug}`}>
                     <img src={`${API_BASE_URL}/${product.image_main}`} className={`${styles.cardImg}`} alt={product.slug}></img>
                 </Link>
-                <div className={`${styles.cardBody}`}>
-                    <p className={`${styles.cardTitle} px-3`}>{product.name}</p>
+                <div className={`${styles.cardBody} px-3`}>
+                    <span className={styles.productLine}>{product.categories[0]}</span>
+                    <p className={`${styles.cardTitle} pt-2`}>{product.name}</p>
+                    <p className={`${styles.productSize}`}>{product.size}</p>
                     <p className={styles.cardHeadline}>{product.headline}</p>
                     <div className="d-flex justify-content-between align-items-baseline py-3">
                     </div>
-                    <div className={`${styles.cardBottom} px-3`}>
+                    <div className={`${styles.cardBottom} px-3 pb-3`}>
                         <p className={styles.cardPrice}>{`$${product.price}` || "Product Description"}</p>
                         <a href="#" className={`${styles.cartBtn} btn`}><LuShoppingCart /></a>
                     </div>
